445b57b9be479e589d9b74d2b776a220fc5c2b5c
Ana Luisa Dias <analudias@live.com>
2021-02-14 17:47:27 -0300

    first commit

---
+++ src/main/java/com/purplepizza/postscrud/PostscrudApplication.java
@@ -0,0 +1,13 @@
+package com.purplepizza.postscrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostscrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostscrudApplication.class, args);
+	}
+
+}
