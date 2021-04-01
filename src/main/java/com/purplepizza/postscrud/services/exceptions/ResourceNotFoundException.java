ee35d23a723a108fde4bc3367e9832e3c87c386f
Ana Luisa Dias <analudias@live.com>
2021-04-01 00:39:20 -0300

    Creating exception treatment

---
+++ src/main/java/com/purplepizza/postscrud/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.purplepizza.postscrud.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
